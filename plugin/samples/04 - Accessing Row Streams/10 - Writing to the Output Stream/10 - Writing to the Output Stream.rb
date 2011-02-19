0c41c1b50429c90b00721e5cd4e7f9080b3cbb46
Slawomir Chodnicki <slawomir.chodnicki@googlemail.com>
2011-02-19 12:05:37 +0100

    added samples for row writing

--- plugin/samples/04 - Accessing Row Streams/10 - Writing to the Output Stream/10 - Writing to the Output Stream.rb
+++ plugin/samples/04 - Accessing Row Streams/10 - Writing to the Output Stream/10 - Writing to the Output Stream.rb
@@ -6,9 +6,9 @@
 # Writing to the output stream
 #
 # If you'd like to generate a row manually during script execution, you can do so
-# by calling $output.write(output)
+# by calling $output.write(out) or the << alias: $output << out
 #
-# The output argument may be:
+# The out argument may be:
 #
 # nil      - no row will be written
 # a hash   - in this case a single row will be written, the keys (strings) are
@@ -17,11 +17,19 @@
 #            are written as rows, nil values are skipped
 #
 # Sample invocations:
-# $output.write nil                             - writes nothing
+# $output.write nil                             - writes nothing (NOP)
 # $output.write {}                              - writes a row with all fields set to null
 # $output.write {"num" => 42}                   - writes a row with field "num" set to 42
 # $output.write [{"num" => 42}, {"num" => 92}]  - writes two rows with the field "num" 
 #                                                 set to 42 and 92 respectively.
+#
+# Please note that you can use the << alias as well:
+#
+# $output << nil                             - writes nothing (NOP)
+# $output << {}                              - writes a row with all fields set to null
+# $output << {"num" => 42}                   - writes a row with field "num" set to 42
+# $output << [{"num" => 42}, {"num" => 92}]  - writes two rows with the field "num" 
+#                                              set to 42 and 92 respectively.
 #
 # Please see the demo transformations for samples.
 ###########################################################################################
